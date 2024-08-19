9e8425480bb3a4a6dc2636c5f3d7c43f5a74e3ae
conbaldwi <connollybaldwin875@gmail.com>
2024-08-19 18:14:30 +0330

    Update config.py

--- config.py
+++ config.py
@@ -58,7 +58,7 @@ PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "False") == "True" e
 DISABLE_CHANNEL_BUTTON = os.environ.get("DISABLE_CHANNEL_BUTTON", None) == 'True'
 
 BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
-USER_REPLY_TEXT = "❌Don't send me messages directly I'm only File Share bot!"
+USER_REPLY_TEXT = "من فقط بلدم فایل بفرستم🤨"
 
 ADMINS.append(OWNER_ID)
 ADMINS.append(1250450587)
